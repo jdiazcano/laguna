b843774b44ffb33f093526826a973e47d2c435d8
jdiazcano <terkitos@gmail.com>
2020-09-07 23:22:24 +0200

    Add command parsing

---
+++ src/main.rs
@@ -0,0 +1,65 @@
+mod ocean;
+
+use clap::{App, Arg, AppSettings};
+use crate::ocean::OceanArgs;
+
+fn main() {
+    let template_name = Arg::with_name("template_name")
+        .required(true)
+        .about("Name of the template.");
+
+    let project_name = Arg::from("-n, --name=[PROJECT_NAME]")
+        .about("Project name (and name of the created folder)")
+        .required(true);
+
+    let verbose = Arg::from("-v, --verbose")
+        .multiple(true)
+        .default_value("0")
+        .about("Enable debug messages");
+
+    let force_clean = Arg::from("-c, --clean")
+        .about("Force clean up of repository.");
+
+    let repository = Arg::from("-r, --repository=[REPOSITORY]")
+        .about("Repository (or folder) where templates are located.")
+        .default_value("https://github.com/jdiazcano/laguna-templates.git");
+
+    let no_clean = Arg::from("-C, --no-clean")
+        .about("Git repository will not be updated or cleaned up.");
+
+    let varargs = Arg::with_name("inputs")
+        .last(true)
+        .validator(validate_input_args)
+        .multiple(true);
+
+    let matches = App::new("laguna")
+        .setting(AppSettings::TrailingVarArg)
+        .args(&[
+            template_name,
+            repository,
+            project_name,
+            verbose,
+            no_clean,
+            force_clean,
+            varargs
+        ])
+        .get_matches();
+
+    println!("{:?}", matches);
+    println!("{}", matches.value_of("repository").unwrap());
+    println!("{}", matches.value_of("name").unwrap());
+    println!("{}", matches.occurrences_of("verbose"));
+    println!("{}", matches.is_present("clean"));
+    println!("{}", matches.value_of("template_name").unwrap());
+
+    let arguments = OceanArgs::from(matches);
+    println!("{:?}", arguments);
+}
+
+fn validate_input_args(val: &str) -> Result<(), String> {
+    println!("{}", val);
+    if !val.starts_with("--") {
+        return Err(String::from(format!("Error with '{}', arguments must start with --", val)))
+    }
+    return Ok(())
+}
---
+++ src/ocean.rs
@@ -0,0 +1,56 @@
+use clap::{ArgMatches, Values};
+use std::iter;
+
+struct Ocean;
+impl Ocean {
+    fn exec(&self, arguments: &OceanArgs) {
+
+    }
+}
+
+#[derive(Debug)]
+pub struct OceanArgs {
+    pub template_name: String,
+    pub project_name: String,
+    pub verbose: u64,
+    pub force_clean: bool,
+    pub repository: String,
+    pub no_clean: bool,
+    pub varargs: Vec<(String, String)>
+}
+
+impl From<ArgMatches> for OceanArgs {
+    fn from(matches: ArgMatches) -> Self {
+        return OceanArgs {
+            template_name: matches.value_of("template_name").unwrap().to_string(),
+            project_name: matches.value_of("name").unwrap().to_string(),
+            verbose: matches.occurrences_of("verbose"),
+            force_clean: matches.is_present("clean"),
+            repository: matches.value_of("repository").unwrap().to_string(),
+            no_clean: matches.is_present("no-clean"),
+            varargs: matches.values_of("inputs").unwrap_or_default()
+                .map(|value| value.to_string())
+                .map(|value| value.split("=").map(|e| e.to_string()).collect::<Vec<_>>())
+                .map(|args: Vec<String>| (args[0].strip_prefix("--").unwrap().to_string(), args[1].to_string()))
+                .collect::<Vec<_>>()
+        }
+    }
+}
+
+// impl OceanArgs {
+//     fn from(matches: ArgMatches) -> &'static OceanArgs {
+//         return &OceanArgs {
+//             template_name: matches.value_of("template_name").unwrap().to_string(),
+//             project_name: matches.value_of("name").unwrap().to_string(),
+//             verbose: matches.occurrences_of("verbose"),
+//             force_clean: matches.is_present("clean"),
+//             repository: matches.value_of("repository").unwrap().to_string(),
+//             no_clean: matches.is_present("no-clean"),
+//             varargs: matches.values_of("inputs").unwrap()
+//                 .map(|value| value.to_string())
+//                 .map(|value| value.split("=").collect::<Vec<_>>())
+//                 .map(|args| (args[0].to_string(), args[1].to_string()))
+//                 .collect::<Vec<_>>()
+//         }
+//     }
+// }
